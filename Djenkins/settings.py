22b60589dc7a9b15c2e2dfec4bb4e0acff3820f8
ljchu <aljchu@163.com>
2014-01-24 17:16:59 +0800

    Add test suits and the code of user contacts .

--- Djenkins/settings.py
+++ Djenkins/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+HERE = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ft',
+    'UserContacts',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,3 +82,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+TEMPLATE_DIRS = (
+    os.path.join(HERE, 'templates').replace('\\','/'),
+)
